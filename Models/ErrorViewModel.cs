8828e620294ea1b1433231d01c36e6974c153a02
Ryan Donnelly <51913413+Linkbit-gif@users.noreply.github.com>
2023-11-17 13:28:54 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gamma.Absorption.Program.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
